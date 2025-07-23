5cee70bbaca9268e4c4876b003ce7ccfb51adced
KusaiAlMalt <215817955+KusaiAlMalt@users.noreply.github.com>
2025-07-23 15:17:10 +0200

    fix: now works despite placeholder

--- update_readme.py
+++ update_readme.py
@@ -2,15 +2,26 @@ from datetime import date
 
 grad_date : date = date(2028, 6, 1)
 today : date = date.today()
-days_left = (grad_date-today).days
+new_day_count = (grad_date - today).days
 
 with open("README.md", "r", encoding="utf-8") as f:
-    content = f.read()
+    content_lines = f.readlines()
 
-new_content = content.replace(
-    "{{ graduationCountdown }}",
-    f"{days_left} day{'s' if days_left != 1 else ''}"
-)
+grad_line_list = content_lines[-1].split()
+
+if new_day_count > 0:
+    grad_line_list[-2] = str(new_day_count)
+    grad_line_list[-1] = f"day{'s' if new_day_count !=1 else ''}"
+else:
+    grad_line_list[-2] = "🎉🎉I have Graduated !!!!🎉🎉"
+    grad_line_list.pop()
+
+old_grad_line = content_lines[-1]
+new_grad_line = " ".join(grad_line_list)
+
+content = "".join(content_lines)
+
+new_content = content.replace(old_grad_line, new_grad_line)
 
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(new_content)
